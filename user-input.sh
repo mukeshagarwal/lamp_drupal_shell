b7fb51be21e412ec5ef82c17cdb773d7a1f92e44
Kuldeep Singh Pal <tkuldeep.singh@innoraft.com>
2014-05-30 13:51:47 +0530

    Again syntax error solves

--- user-input.sh
+++ user-input.sh
@@ -5,6 +5,8 @@ read -p "Enter Web user who will run web server:    " user
 read -p "Enter domain for project:    " domain
 read -p "Enter password for mysql root user:    " db_pass
 read -p "Enter Project root directory [drupal or drupal/docroot]:	" project_dir
+echo "Database Credentials"
+read -p "Name of database:    " db_name
 
 # Take proejct url
 read -p "Give the git url of your project:    " project_url
@@ -15,9 +17,6 @@ if [ $branch = Y ] || [ $branch = y ]
  read -p  "Specify branch:	" 	project_branch
 fi
 
-echo "Database Credentials"
-read -p "Name of database:    " db_name
-read -p "Password of database of this machine:    " db_pass
 
 project_remote_ip='none'
 project_remote_user='none'
@@ -39,8 +38,7 @@ fi
 # Run Script for setting up LAMP
 sh ~/lamp_drupal_shell/lamp-setup.sh $user $domain $db_pass $project_dir
 # Run script for setting up project
-sh ~/lamp_drupal_shell/project-setup.sh $project_url $branch $project_branch $project_dir $db_name
- $db_pass $method $files_url $db_url $project_remote_ip $project_remote_user $secure_key $user
+sh ~/lamp_drupal_shell/project-setup.sh $project_url $branch $project_branch $project_dir $db_name $db_pass $method $files_url $db_url $project_remote_ip $project_remote_user $secure_key $user
 # RUn script for setting post fix mail server
 sh ~/lamp_drupal_shell/mailserver-postfix.sh
 
